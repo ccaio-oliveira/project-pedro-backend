d729879079df0f5698177802fd8e4253a0672410
Caio Oliveira <ccaio.oliveira83@gmail.com>
2024-02-08 14:16:25 -0300

    ajustes sanctum

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -44,6 +44,11 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
+
+        'sanctum' => [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
     ];
 
     /**
