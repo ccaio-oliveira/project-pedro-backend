e86104604a6f619a0eb1a64d5101ca70bd830773
Caio Oliveira <ccaio.oliveira83@gmail.com>
2024-02-09 21:43:07 -0300

    feat: add cors middleware

--- routes/api.php
+++ routes/api.php
@@ -18,18 +18,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/validaSessao', [AuthController::class, 'isLogged']);
-    Route::get('/usuarios', [UserController::class, 'getUsuarios']);
-    Route::get('/relatorios', [RelatorioController::class, 'getRelatorios']);
-    Route::post('/relatorios', [RelatorioController::class, 'createRelatorio']);
-    Route::get('/relatorios/download/{id}', [FileController::class, 'getFileDownload']);
-    Route::get('/relatorios/{relatorio_id}/{atrelado_a}', [RelatorioController::class, 'changeViewed']);
+Route::group(['middleware' => 'cors'], function () {
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/validaSessao', [AuthController::class, 'isLogged']);
+        Route::get('/usuarios', [UserController::class, 'getUsuarios']);
+        Route::get('/relatorios', [RelatorioController::class, 'getRelatorios']);
+        Route::post('/relatorios', [RelatorioController::class, 'createRelatorio']);
+        Route::get('/relatorios/download/{id}', [FileController::class, 'getFileDownload']);
+        Route::get('/relatorios/{relatorio_id}/{atrelado_a}', [RelatorioController::class, 'changeViewed']);
+    });
+
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/loggout', [AuthController::class, 'logout']);
+
+    Route::post('/forgotPassword', [AuthController::class, 'forgotPassword']);
+    Route::post('/resetPassword/checkToken', [AuthController::class, 'checkToken']);
+    Route::post('/resetPassword', [AuthController::class, 'resetPassword']);
 });
-
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/loggout', [AuthController::class, 'logout']);
-
-Route::post('/forgotPassword', [AuthController::class, 'forgotPassword']);
-Route::post('/resetPassword/checkToken', [AuthController::class, 'checkToken']);
-Route::post('/resetPassword', [AuthController::class, 'resetPassword']);
