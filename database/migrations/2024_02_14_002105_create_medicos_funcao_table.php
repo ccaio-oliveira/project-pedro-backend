0265b2b820d71105911b824bd826745bb08fa2ce
Caio Oliveira <ccaio.oliveira83@gmail.com>
2024-02-17 11:49:23 -0300

    feat: atualizar tabela de função

--- database/migrations/2024_02_14_002105_create_medicos_funcao_table.php
+++ database/migrations/2024_02_14_002105_create_medicos_funcao_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -14,12 +15,45 @@ return new class extends Migration
         Schema::create('medicos_funcao', function (Blueprint $table) {
             $table->integer('id', true);
             $table->string('funcao');
-            $table->integer('medico_id');
-
-            $table->foreign('medico_id')->references('id')->on('usuarios');
 
             $table->engine = 'InnoDB';
         });
+
+        $specialties = [
+            'Anestesiologia',
+            'Cardiologia',
+            'Cirurgia Geral',
+            'Cirurgia Plástica',
+            'Cirurgia Vascular',
+            'Clínica Médica',
+            'Dermatologia',
+            'Endocrinologia',
+            'Gastroenterologia',
+            'Geriatria',
+            'Ginecologia',
+            'Hematologia',
+            'Infectologia',
+            'Nefrologia',
+            'Neurologia',
+            'Nutrologia',
+            'Oftalmologia',
+            'Oncologia',
+            'Ortopedia',
+            'Otorrinolaringologia',
+            'Pediatria',
+            'Pneumologia',
+            'Proctologia',
+            'Psiquiatria',
+            'Radiologia',
+            'Reumatologia',
+            'Urologia'
+        ];
+
+        foreach ($specialties as $specialty) {
+            DB::table('medicos_funcao')->insert([
+                'funcao' => $specialty,
+            ]);
+        }
     }
 
     /**
