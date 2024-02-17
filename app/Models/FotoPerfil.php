751e1045f568c82b81a5874f5e5754f1f07c5141
Caio Oliveira <ccaio.oliveira83@gmail.com>
2024-02-17 11:25:19 -0300

    feat: adicionar tabela de perfil

---
+++ app/Models/FotoPerfil.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FotoPerfil extends Model
+{
+    use HasFactory;
+
+    protected $table = 'usuarios_foto_perfil';
+}
