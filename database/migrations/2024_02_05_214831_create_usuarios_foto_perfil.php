392f8ce4a16f214139cd7409573650b37f9a0b3c
Caio Oliveira <ccaio.oliveira83@gmail.com>
2024-02-17 11:15:39 -0300

    feat: adicionar migration da foto de perfil

---
+++ database/migrations/2024_02_05_214831_create_usuarios_foto_perfil.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('usuarios_foto_perfil', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->binary('foto');
+            $table->timestamps();
+
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('usuarios_foto_perfil');
+    }
+};
