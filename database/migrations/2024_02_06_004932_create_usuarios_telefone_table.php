bec09ff60b66b86624e2f0465228d1379a418546
Caio Oliveira <ccaio.oliveira83@gmail.com>
2024-02-23 06:43:04 -0300

    fix: corrigir migration

--- database/migrations/2024_02_06_004932_create_usuarios_telefone_table.php
+++ database/migrations/2024_02_06_004932_create_usuarios_telefone_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -15,12 +16,46 @@ return new class extends Migration
             $table->integer('id', true);
             $table->integer('usuario_id');
             $table->string('telefone', 50);
+            $table->string('tipo_telefone', 50)->nullable();
             $table->timestamps();
 
             $table->foreign('usuario_id')->references('id')->on('usuarios');
 
             $table->engine = 'InnoDB';
         });
+
+        DB::table('usuarios_telefone')->insert([
+            [
+                'usuario_id' => 1,
+                'telefone' => '5574998059407',
+                'tipo_telefone' => 'celular'
+            ],
+            [
+                'usuario_id' => 1,
+                'telefone' => '5574998059407',
+                'tipo_telefone' => 'whatsapp'
+            ],
+            [
+                'usuario_id' => 2,
+                'telefone' => '5574998059407',
+                'tipo_telefone' => 'celular'
+            ],
+            [
+                'usuario_id' => 2,
+                'telefone' => '5574998059407',
+                'tipo_telefone' => 'whatsapp'
+            ],
+            [
+                'usuario_id' => 3,
+                'telefone' => '5574998059407',
+                'tipo_telefone' => 'celular'
+            ],
+            [
+                'usuario_id' => 3,
+                'telefone' => '5574998059407',
+                'tipo_telefone' => 'whatsapp'
+            ],
+        ]);
     }
 
     /**
